6eb740cbf7c698839059b9dc37f93a770da28b5f
lioujhihjie1 <30904255+lioujhihjie1@users.noreply.github.com>
2017-08-16 17:38:14 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+<script>
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('myVideo', {
+      events: {
+          'onReady': onPlayerReady
+      }
+  });
+}
+
+function onPlayerReady() {
+  player.playVideo();
+  // Mute!
+  player.mute();
+}
+</script>
