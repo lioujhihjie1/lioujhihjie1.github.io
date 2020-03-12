fd2ad8e1f35c6d0c02533ce03ecc9b75c12f8276
JJ <lioujhihjie@gmail.com>
2020-03-12 22:46:42 +0800

    Projects Update

--- main.js
+++ main.js
@@ -73,3 +73,79 @@ anime({
   delay: 200, // All properties except 'scale' inherit 250ms delay
   loop: 2
 });
+
+
+anime({
+  targets: 'div.logo3.box',
+  rotate: [{
+    value: 720,
+    duration: 800,
+    delay:3200,
+    easing: 'easeInOutSine'
+  },
+  {
+    value: 0,
+    duration: 800,
+    delay:3200,
+    easing: 'easeInOutSine'
+  }
+],
+  scale: [{
+    value: 0,
+    duration:0,
+    delay:0,
+    easing: 'easeInOutQuart'
+  },
+  {
+    value: 1,
+    duration: 800,
+    delay: 3200,
+    easing: 'easeInOutQuart'
+  },
+  {
+    value: 0,
+    duration: 800,
+    delay:3200,
+    easing: 'easeInOutQuart'
+  }],
+  delay: 200, // All properties except 'scale' inherit 250ms delay
+  loop: 2
+});
+
+
+
+anime({
+  targets: 'div.logo4.box',
+  rotate: [{
+    value:0,
+    duration: 800,
+    delay:200,
+    easing: 'easeInOutSine'
+  },
+  {
+    value: 720,
+    duration: 800,
+    delay:2000,
+    easing: 'easeInOutSine'
+  }],
+  scale: [{
+    value: 1,
+    duration:0,
+    delay:0,
+    easing: 'easeInOutQuart'
+  },
+  {
+    value: 0,
+    duration: 800,
+    delay: 3200,
+    easing: 'easeInOutQuart'
+  },
+  {
+    value: 1,
+    duration: 800,
+    delay:3200,
+    easing: 'easeInOutQuart'
+  }],
+  delay: 200, // All properties except 'scale' inherit 250ms delay
+  loop: 2
+});
